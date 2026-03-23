0f237a0525f8f83c5bf7a8895dcc273397e4016d
seojin <ggomingo1242@gmail.com>
2026-03-24 02:10:52 +0900

    3주차 과제

---
+++ Assets/Scripts/BulletFire.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour
+{
+    public GameObject bulletObject;
+    public GameObject bulletFireObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isfire = Input.GetButtonDown("Fire1");
+        if (isfire)
+        {
+            GameObject bullet = Instantiate(bulletObject);
+            bullet.transform.position = bulletFireObject.transform.position;
+        }
+    }
+}
